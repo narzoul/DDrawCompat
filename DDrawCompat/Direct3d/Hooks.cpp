0ada5acea710868f882acaa6230c5bd60c0031fe
narzoul <narzoul@users.noreply.github.com>
2021-05-01 14:11:08 +0200

    Fixed crash at startup in Debug build
    See issue #96.

--- DDrawCompat/Direct3d/Hooks.cpp
+++ DDrawCompat/Direct3d/Hooks.cpp
@@ -28,7 +28,11 @@ namespace
 		CompatPtr<TDirect3d> d3d;
 		HRESULT result = dd->QueryInterface(&dd, Compat::getIntfId<TDirect3d>(),
 			reinterpret_cast<void**>(&d3d.getRef()));
-		if (FAILED(result))
+		if (SUCCEEDED(result))
+		{
+			CompatVtable<Vtable<TDirect3d>>::s_origVtable = *d3d.get()->lpVtbl;
+		}
+		else
 		{
 			Compat::Log() << "ERROR: Failed to create a Direct3D object for hooking: " << Compat::hex(result);
 		}
