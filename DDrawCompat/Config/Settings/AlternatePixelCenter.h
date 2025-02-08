c9ea21034060066bfd5bb5fb0f230992fc2655b5
narzoul <narzoul@users.noreply.github.com>
2025-02-08 11:15:58 +0100

    Added AlternatePixelCenter=custom setting
    See issue #429.

--- DDrawCompat/Config/Settings/AlternatePixelCenter.h
+++ DDrawCompat/Config/Settings/AlternatePixelCenter.h
@@ -10,8 +10,22 @@ namespace Config
 		{
 		public:
 			AlternatePixelCenter()
-				: MappedSetting("AlternatePixelCenter", "off", { {"off", 0.0f}, {"on", -0.5f} })
+				: MappedSetting("AlternatePixelCenter", "off", { {"off", 0.0f}, {"on", -0.5f}, {"custom", INFINITY} })
 			{
+			}
+
+			float get() const
+			{
+				return m_value == INFINITY ? m_param / 100.0f : m_value;
+			}
+
+			virtual ParamInfo getParamInfo() const override
+			{
+				if (INFINITY == m_value)
+				{
+					return { "Offset", -100, 100, -50 };
+				}
+				return {};
 			}
 		};
 	}
